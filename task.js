838893010623618a5a3798366e43da9a13742852
manfredasmi <manfredasmi@gmail.com>
2018-11-11 18:10:02 +0200

    2nd commit

--- task.js
+++ task.js
@@ -13,16 +13,36 @@ describe('Test', function() {
         browser.findElement(By.css('[type="submit"]')).click();
         browser.wait(EC.presenceOf($('[href="https://mail.google.com"]')), 10000);
         browser.findElement(By.css('[href="https://mail.google.com"]')).click();
+
+        // istrinti
+        // browser.get('http://www.gmail.com/');
+        // browser.manage().window().maximize();
+        // iki cia sita dali
         browser.findElement(By.css('[type="email"]')).sendKeys('vardenissd@gmail.com');
+        browser.wait(EC.presenceOf($('#identifierNext')), 10000);
         browser.findElement(By.css('#identifierNext')).click();
-        browser.wait(EC.presenceOf($('[type="password"]')), 10000);
+        browser.sleep(2000);
         browser.findElement(By.css('[type="password"]')).sendKeys('LabaDiena12');
         browser.findElement(By.css('#passwordNext')).click();
-        browser.wait(EC.presenceOf($('[email="noreply@telia.lt"]')), 60000);
-        browser.findElement(By.css('[email="noreply@telia.lt"]')).click();
+        // wait for email to arrive
+        browser.wait(EC.presenceOf($('[email="noreply@telia.lt"]')), 150000);
+        let list = $$('[email="noreply@telia.lt"]');
+        list.get(1).click();
         browser.wait(EC.presenceOf($('[href*="http://informing.mailer.telia.lt/links/view/"]')), 10000);
-        browser.findElement(By.css('[href*="http://informing.mailer.telia.lt/links/view/"]')).click();
+        let list1 = $$('[href*="http://informing.mailer.telia.lt/links/view/"]');
+        list1.get(1).click();
+        // switching to another pop up window
+        browser.getAllWindowHandles().then(function(handles) {
+            browser.switchTo().window(handles[1]).then(function() {
+              browser.wait(EC.presenceOf($('[type="password"]')), 10000); // neranda sito elemento
+              browser.findElement(By.css('[type="password"]')).sendKeys('LabaDiena123');
+              browser.findElement(By.css('.checkbox__content')).click();
+              browser.findElement(By.css('.btn.btn-primary')).click();
+              browser.findElement(By.css('.btn.btn-primary')).click();
+              browser.findElement(By.cssContainingText('span', 'Uždaryti')).click();
+            });
+        });
 
-        browser.sleep(10000);
+
     });
 });
